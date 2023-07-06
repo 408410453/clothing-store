d217191724215f184832629935019e6682bef5ff
408410453 <sam29444237@gmail.com>
2023-07-07 01:27:21 +0800

    add express.js

---
+++ public/javascripts/shop.js
@@ -0,0 +1,22 @@
+let search = document.querySelector(".topbar_list_search");
+let search_icon = document.querySelector(".topbar_list_search_icon");
+let search_input = document.querySelector(".topbar_list_search_input");
+let search_close = document.querySelector(".topbar_list_search_close");
+let search_delete = document.querySelector(".topbar_list_search_delete");
+
+search_icon.addEventListener("click",()=>{
+    search.classList.add("topbar_list_search-open");
+    search_input.focus();
+})
+
+search_close.addEventListener("click",()=>{
+    search.classList.remove("topbar_list_search-open");
+    search_input.value="";
+})
+
+search_delete.addEventListener("click",()=>{
+    search_input.value="";
+    search_input.focus();
+})
+
+
